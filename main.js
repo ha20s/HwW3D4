46dc476d9da911aa425695aacda9109e1c77dd38
Hasna <hassna.ahmad98@gmail.com>
2024-09-12 01:19:25 +0300

    hover on title

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let name = document.getElementById("name")
+let nameSaved = name.innerText; 
+name.onmouseover = function(){this.innerText = 'HASNA AHMED'; } ;
+name.onmouseout = function(){this.innerText = nameSaved;};
+
+
+
+let rule = document.getElementById("rule")
+let ruleSaved = rule.innerText; 
+rule.onmouseover = function(){this.innerText = 'IOS DEVELOPER'; } ;
+rule.onmouseout = function(){this.innerText = ruleSaved;};
+
+
+
+
+
