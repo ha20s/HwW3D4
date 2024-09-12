397c5727d33b4da7468e7f743e134a7c59f3963a
Hasna <hassna.ahmad98@gmail.com>
2024-09-12 09:39:06 +0300

    not all the hovers

--- main.js
+++ main.js
@@ -11,7 +11,13 @@ let ruleSaved = rule.innerText;
 rule.onmouseover = function(){this.innerText = 'IOS DEVELOPER'; } ;
 rule.onmouseout = function(){this.innerText = ruleSaved;};
 
+let profile = document.getElementById("profile")
+let porfileSaved = profile.innerText
+profile.onmouseover = function(){this.innerText = 'Recent computer science graduate and iOS developer, eager to contribute and develop skills in a collaborative team Seeking to gain practical experience and participate in innovative projects within a stimulating work environment.'}
+profile.onmouseout = function(){this.innerText = porfileSaved;};
 
 
-
-
+let phone = document.getElementById("phone")
+let phoneSaved = phone.innerText
+profile.onmouseover = function(){this.innerText = '+955 542400000';};
+profile.onmouseout = function(){this.innerText = phoneSaved;};
